4c1d3d0e35c8563724c216e591e6b12561271761
Mallikarjun-Java-Dev <mallikarjunnarasagonda@gmail.com>
2026-07-18 15:38:51 +0530

    Clean up duplicate demo folder structure

--- demo/src/main/java/com/Demo/repository/AdminRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Demo.dto.Admin;
-
-public interface AdminRepo extends JpaRepository<Admin,Integer>{
-
-}
--- demo/src/main/java/com/Demo/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Demo.dto.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
